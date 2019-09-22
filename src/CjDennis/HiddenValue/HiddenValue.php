792a05b6cf2012a540d89a3a5264101f076b4550
Daniel Klein <danielklein@airpost.net>
2019-09-22 16:59:48 +1000

    Pass the test

--- src/CjDennis/HiddenValue/HiddenValue.php
+++ src/CjDennis/HiddenValue/HiddenValue.php
@@ -1,24 +1,46 @@
 <?php
 namespace CjDennis\HiddenValue;
 
+use stdClass;
+
 trait HiddenValue {
   protected function hidden_value($name = null, $value = null) {
     static $keys = [];
     static $values = [];
+    static $named_values = [];
 
     $key = array_search($this, $keys, true);
     if (func_num_args() < 2) {
       if ($key !== false) {
-        $value = $values[$key];
+        if ($name === null) {
+          $value = $values[$key];
+        }
+        else {
+          $value = $named_values[$key]->$name;
+        }
       }
     }
     else {
       if ($key !== false) {
-        $values[$key] = $value;
+        if ($name === null) {
+          $values[$key] = $value;
+        }
+        else {
+          $named_values[$key]->$name = $value;
+        }
       }
       else {
         $keys[] = $this;
-        $values[] = $value;
+        if ($name === null) {
+          $values[] = $value;
+          $named_values[] = new stdClass();
+        }
+        else {
+          $values[] = null;
+          $std_class = new stdClass();
+          $std_class->$name = $value;
+          $named_values[] = $std_class;
+        }
       }
     }
     return $value;
