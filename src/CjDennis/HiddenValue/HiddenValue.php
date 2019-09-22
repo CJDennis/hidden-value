6ee4eb32e686c132277766dfdc784647e4337492
Daniel Klein <danielklein@airpost.net>
2019-09-22 16:17:27 +1000

    Pass the test

--- src/CjDennis/HiddenValue/HiddenValue.php
+++ src/CjDennis/HiddenValue/HiddenValue.php
@@ -2,4 +2,25 @@
 namespace CjDennis\HiddenValue;
 
 trait HiddenValue {
+  protected function hidden_value($name = null, $value = null) {
+    static $keys = [];
+    static $values = [];
+
+    $key = array_search($this, $keys, true);
+    if (func_num_args() < 2) {
+      if ($key !== false) {
+        $value = $values[$key];
+      }
+    }
+    else {
+      if ($key !== false) {
+        $values[$key] = $value;
+      }
+      else {
+        $keys[] = $this;
+        $values[] = $value;
+      }
+    }
+    return $value;
+  }
 }
--- tests/unit/CjDennis/HiddenValue/ExampleClass.php
+++ tests/unit/CjDennis/HiddenValue/ExampleClass.php
@@ -3,4 +3,13 @@ namespace CjDennis\HiddenValue;
 
 class ExampleClass {
   use HiddenValue;
+
+  public function save(string $name, $value) {
+    $this->hidden_value($name, $value);
+  }
+
+  public function load(string $name) {
+    $value = $this->hidden_value($name);
+    return $value;
+  }
 }
