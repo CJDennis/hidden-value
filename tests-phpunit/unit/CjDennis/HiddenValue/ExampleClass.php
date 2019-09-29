52d48889ac18930eb8046e560417ba890acf4ab1
Daniel Klein <danielklein@airpost.net>
2019-09-29 11:58:15 +1000

    Move PHPUnit tests

---
+++ tests-phpunit/unit/CjDennis/HiddenValue/ExampleClass.php
@@ -0,0 +1,24 @@
+<?php
+namespace CjDennis\HiddenValue;
+
+class ExampleClass {
+  use HiddenValue;
+
+  public function save(string $name, $value) {
+    $this->hidden_value($name, $value);
+  }
+
+  public function load(string $name) {
+    $value = $this->hidden_value($name);
+    return $value;
+  }
+
+  public function save_nameless($value) {
+    $this->hidden_value(null, $value);
+  }
+
+  public function load_nameless() {
+    $value = $this->hidden_value(null);
+    return $value;
+  }
+}
---
+++ tests-phpunit/unit/CjDennis/HiddenValue/HiddenValueTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace CjDennis\HiddenValue;
+
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/** @covers \CjDennis\HiddenValue\HiddenValue */
+class HiddenValueTest extends TestCase {
+  public function testShouldUseTheHiddenValueTraitInTheExampleClass() {
+    $this->assertContains(HiddenValue::class, class_uses(ExampleClass::class));
+  }
+
+  public function testShouldRetrieveASavedValue() {
+    $example_class = new ExampleClass();
+    $value = new stdClass();
+    $example_class->save('foobar', $value);
+    $this->assertSame($value, $example_class->load('foobar'));
+  }
+
+  public function testShouldRetrieveAnUnnamedSavedValue() {
+    $example_class = new ExampleClass();
+    $value = new stdClass();
+    $example_class->save_nameless($value);
+    $this->assertSame($value, $example_class->load_nameless());
+  }
+
+  public function testShouldStoreMultipleDifferentValues() {
+    $example_class = new ExampleClass();
+    $value = new stdClass();
+    $value_1 = new stdClass();
+    $value_2 = new stdClass();
+    $example_class->save('foo', $value_1);
+    $example_class->save_nameless($value);
+    $example_class->save('bar', $value_2);
+    $this->assertSame($value_1, $example_class->load('foo'), 'The correct value of $foo was not retrieved');
+    $this->assertSame($value, $example_class->load_nameless(), 'The correct nameless value was not retrieved');
+    $this->assertSame($value_2, $example_class->load('bar'), 'The correct value of $bar was not retrieved');
+  }
+
+  public function testShouldStoreDifferentValuesForDifferentInstances() {
+    $example_class_1 = new ExampleClass();
+    $example_class_2 = new ExampleClass();
+    $value_1a = new stdClass();
+    $value_1b = new stdClass();
+    $value_2a = new stdClass();
+    $value_2b = new stdClass();
+    $example_class_1->save_nameless($value_1a);
+    $example_class_1->save('foo', $value_1b);
+    $example_class_2->save('bar', $value_2a);
+    $example_class_2->save_nameless($value_2b);
+    $this->assertSame($value_1a, $example_class_1->load_nameless(), 'The correct nameless value for example 1 was not retrieved');
+    $this->assertSame($value_1b, $example_class_1->load('foo'), 'The correct value of $foo for example 1 was not retrieved');
+    $this->assertSame($value_2a, $example_class_2->load('bar'), 'The correct value of $bar for example 2 was not retrieved');
+    $this->assertSame($value_2b, $example_class_2->load_nameless(), 'The correct nameless value for example 2 was not retrieved');
+  }
+}
