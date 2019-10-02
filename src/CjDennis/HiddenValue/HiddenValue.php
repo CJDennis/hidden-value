ddf35c7ee3b9958ac1984dbbba0420cada6dfd39
Daniel Klein <danielklein@airpost.net>
2019-10-02 22:23:00 +1000

    Pass the tests

--- src/CjDennis/HiddenValue/HiddenValue.php
+++ src/CjDennis/HiddenValue/HiddenValue.php
@@ -1,9 +1,33 @@
 <?php
 namespace CjDennis\HiddenValue;
 
+use ReflectionClass;
+use ReflectionException;
 use stdClass;
 
 trait HiddenValue {
+  /**
+   * @param null $func_get_args Call `$this->adopt_parent(...func_get_args());` from __construct().
+   * @throws ReflectionException
+   */
+  protected function adopt_parent($func_get_args = null) {
+    $parent = new parent(...func_get_args());
+    $this->hidden_value(null, $parent);
+
+    foreach ($parent as $name => $value) {
+      $this->$name = $value;
+    }
+
+    $reflection_class = new ReflectionClass($parent);
+    $reflection_properties = $reflection_class->getProperties();
+    foreach ($reflection_properties as $reflection_property) {
+      $reflection_property->setAccessible(true);
+      $name = $reflection_property->getName();
+      $value = $reflection_property->getValue($parent);
+      $this->$name = $value;
+    }
+  }
+
   protected function hidden_value($name = null, $value = null) {
     static $data = [];
 
